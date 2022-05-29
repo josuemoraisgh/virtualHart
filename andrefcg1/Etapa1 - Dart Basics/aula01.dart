06218e5e0dbeea893772d291abfaf36870470ba2
andrefcg1 <65246453+andrefcg1@users.noreply.github.com>
2022-05-28 21:16:43 -0300

    Pratica dart 1/2
    Aula 03 - Primeiros comandos

--- andrefcg1/Etapa1 - Dart Basics/aula01.dart
+++ andrefcg1/Etapa1 - Dart Basics/aula01.dart
@@ -1,1 +1,16 @@
-void main(List<String> args) {}
+//global
+import 'dart:ffi';
+
+var c = 2;
+
+void main() {
+  String d = "ola";
+  print(d);
+  print(soma(b: c, a: 3));
+}
+
+int soma({final int a = 8, final int b = 12}) {
+  var c;
+  c = b + a;
+  return c;
+}
